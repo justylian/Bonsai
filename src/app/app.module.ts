5d8453d676b82c8729ff8562b14242a04c7bce6b
justylian <steliosxenakis22@gmail.com>
2020-06-09 21:31:11 +0300

    Initial Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { AboutComponent } from './about/about.component';
+import { WorkComponent } from './work/work.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainComponent,
+    AboutComponent,
+    WorkComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/main/main.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { NONE_TYPE } from '@angular/compiler/src/output/output_ast';
+
+declare var require: any;
+declare var $: any;
+declare var jQuery: any;
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss']
+})
+export class MainComponent implements OnInit {
+
+  constructor() {
+
+   }
+
+  ngOnInit() {
+
+  }
+  public crossout(i){
+    $("#main-menu-line-"+i).hide();
+    $("#main-menu-line-"+i).animate({
+      marginLeft: "-=50",
+    }, 100);
+    $("#main-menu-li-"+i).animate({
+      fontSize: "25",
+    }, 100);
+  }
+  public cross(i){
+    $("#main-menu-line-"+i).show();
+      $("#main-menu-line-"+i).animate({
+        marginLeft: "+=50",
+      }, 100);
+      $("#main-menu-li-"+i).animate({
+        fontSize: "27",
+      }, 100);
+  }
+}
