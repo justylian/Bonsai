f9ae03f53e7518ba6cc5c5c79d5ea54f73a986d0
justylian <steliosxenakis22@gmail.com>
2020-06-19 13:31:50 +0300

    Map Marker

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import Map from 'ol/map';
 import View from 'ol/View';
+import Feature from 'ol/Feature';
+
 import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
 import Style from 'ol/style/Style';
 import Icon from 'ol/style/Icon';
 import OSM from 'ol/source/OSM';
 import * as olProj from 'ol/proj';
 import TileLayer from 'ol/layer/Tile';
 import {defaults as defaultInteractions, MouseWheelZoom} from 'ol/interaction.js';
-
+import Point from 'ol/geom/Point';
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -21,13 +24,31 @@ export class MapComponent implements OnInit {
   }
 
   public ngOnInit() {
+    const markerSource = new VectorSource();
+
+    var markerStyle = new Style({
+      image: new Icon(/** @type {olx.style.IconOptions} */ ({
+        anchor: [0.5, 46],
+        anchorXUnits: 'fraction',
+        anchorYUnits: 'pixels',
+        src: "../../assets/images/background/marker.png"
+      }))
+    });
+
+
+
     let map = new Map({
       target: 'map',
       layers: [
         new TileLayer({
           source: new OSM()
-        })
-      ],
+        }),
+
+      new VectorLayer({
+        source: markerSource,
+        style: markerStyle,
+      })
+    ],
       view: new View({
         center: olProj.fromLonLat([24.3, 35.40]),
         zoom: 11
@@ -37,6 +58,26 @@ export class MapComponent implements OnInit {
 
 
     });
+
+
+      console.log('lon:');
+      console.log('lat:');
+
+      var iconFeatures = [];
+
+      var iconFeature = new Feature({
+        geometry: new Point(olProj.transform([24.201062, 35.375429], 'EPSG:4326',
+          'EPSG:3857')),
+        name: 'Null Island',
+        population: 4000,
+        rainfall: 500
+      });
+
+      markerSource.addFeature(iconFeature);
+
+
+
+
 
 
 
