1c3063c3d99204979dc4932f1e972ecc1dd75d66
justylian <steliosxenakis22@gmail.com>
2020-07-03 01:10:41 +0300

    Visibility of Arrow

--- src/app/services/language.service.ts
+++ src/app/services/language.service.ts
@@ -13,6 +13,11 @@ export class LanguageService {
     if(this.langState==="en"){
       console.log("SVD");
       $('#language').text('EN');
+      $('#main-menu-li-1').text('Το Ανθοπωλείο');
+      $('#main-menu-li-2').text('');
+      $('#main-menu-li-3').text('Τοποθεσία');
+      $('#main-menu-li-4').text('Επικοινωνία');
+
       this.langState="el";
     }
     else{
