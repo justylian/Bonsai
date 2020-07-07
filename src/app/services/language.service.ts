535e417c0d57944c553e36ab355c57773be9295c
justylian <steliosxenakis22@gmail.com>
2020-07-08 00:00:20 +0300

    Greek Version

--- src/app/services/language.service.ts
+++ src/app/services/language.service.ts
@@ -14,6 +14,9 @@ export class LanguageService {
 
       $('#language').text('EN');
       /*ΗΟΜΕ PAGE*/
+      $("#main-menu-li-0").contents().filter(function(){
+        return this.nodeType == 3;
+      })[0].nodeValue = 'Αρχική'
       $("#main-menu-li-1").contents().filter(function(){
         return this.nodeType == 3;
       })[0].nodeValue = 'Το Ανθοπωλείο'
@@ -28,10 +31,37 @@ export class LanguageService {
       })[0].nodeValue = 'Επικοινωνία'
       //$('#main-menu-li-1').text('Το Ανθοπωλείο');
       $('#main-menu ul').animate({
-        left: "30%"
+        left: "24%"
       },0);
       $('#main-header h4').text('To Bonsai είναι ένα ανθοπωλείο στις Βρύσες Αποκορώνου. Από το 1992 που ξεκινήσαμε τη λειτουργία μας συνεχίζουμε να παρέχουμε φρέσκα άνθη, φυτά και ξεχωριστές δημιουργίες για κάθε ιδιαίτερη στιγμή σας!');
+
       /*OUR STORY*/
+      $('.about-text h2').text('Η Ιστορία μας');
+      $('#about-description').text('Ξεκινώντας το 1992, δημιουργήσαμε το ανθοπωλείο, πρώτο στο είδος του στην περιοχή. Συνεχίζουμε να παρέχουμε για τις ανάγκες του πελάτη φρέσκα και υψηλής ποιότητας άνθη και φυτά. Με περισσότερα απο 25 χρόνια εμπειρίας ομορφαίνωντας τις ιδιαίτερες στιγμές σας με στολισμούς που μένουν αξέχαστοι.Αφήστε τις ημέρες που ονειρεύεστε να πραγματοποιηθούν με τον πιο δημιουργικό τρόπο!');
+      /*MAP*/
+      $('#work h2').text('Δημιουργίες');
+      $('#work-list-1').text('Όλα');
+      $('#work-list-2').text('Γάμοι');
+      $('#work-list-3').text('Βαφτίσεις');
+
+    /*MAP*/
+    $('#map h2').text('Τοποθεσία');
+    $('#map h4').html('Κεντρική Πλατεία<br> Βρύσες Αποκορώνου<br> Χανιά<br> 73007');
+
+    /*CONTACT*/
+    $('#contact h2').text('Επικοινωνία');
+    $('#fname-label').text('Όνομα');
+    $('#fname').attr("placeholder", 'Όνομα');
+
+    $('#lname-label').text('Επώνυμο');
+    $('#lname').attr("placeholder", "Επώνυμο");
+
+    $('#message').text('Μήνυμα');
+    $('#subject').attr("placeholder",'Μήνυμα');
+
+    $('#submit').attr("value", "Αποστολή");
+
+
 
 
 
