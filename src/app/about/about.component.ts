656eda7cd4d134f6da311a000e6fa432f547a153
justylian <steliosxenakis22@gmail.com>
2020-07-08 19:26:42 +0300

    Slideshow About

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var require: any;
+declare var $: any;
+declare var jQuery: any;
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent implements OnInit {
 
+export class AboutComponent implements OnInit {
   constructor() { }
 
+
   ngOnInit() {
+
+
   }
 
 }
